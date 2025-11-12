2aed3a9f83ef3c76022be4434aa750a256f22641
kevinliang73741 <sl73741@uga.edu>
2025-11-12 14:34:54 -0500

    No need to set R_HOME in submodule

--- gp_utils/models/models.py
+++ gp_utils/models/models.py
@@ -1,12 +1,10 @@
-import os
-os.environ['R_HOME'] = os.getenv('R_HOME')
+import numpy as np
 
 import rpy2.robjects as robjects
 from rpy2.robjects import default_converter
 from rpy2.robjects.conversion import localconverter
 from rpy2.robjects import numpy2ri
 
-import numpy as np
 from sklearn.base import BaseEstimator, RegressorMixin
 
 ################
